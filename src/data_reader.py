bdcfed9f1b5da6da8cadd6c83e8b3d8e87a57e04
wassim <wassim.fourati@outlook.com>
2019-04-28 22:23:30 +0100

    adding technical indicators

--- src/data_reader.py
+++ src/data_reader.py
@@ -4,6 +4,18 @@ from pandas_datareader import data
 start_date = '2010-01-01'
 end_date = '2016-12-31'
 
+def get_data(index = 'INPX', start_date='2010-01-01', end_date = '2016-12-31' ):
+	panel_data = data.DataReader(index, 'yahoo', start_date, end_date)
+	return panel_data
 
-panel_data = data.DataReader('INPX', 'yahoo', start_date, end_date)
-print panel_data
+def save_data(index = 'INPX', start_date='2010-01-01', end_date = '2016-12-31' ):
+	panel_data = get_data(index, start_date, end_date)
+	out_path= "data/" + index + ".csv"
+	print panel_data
+	panel_data.to_csv(out_path)
+	
+
+def import_data(index = 'INPX', start_date='2010-01-01', end_date = '2016-12-31' ):
+	input_path= "data/" + index + ".csv"
+	panel_data = pd.read_csv(input_path, index_col="Date")
+	return panel_data
---
+++ src/technical_indicators.py
@@ -0,0 +1,102 @@
+import numpy as np 
+import pandas as pd
+import catch_errors
+
+
+
+
+
+def simple_moving_average(data, n):
+    """Calculate the moving average for the given data.
+    
+    :param data: pandas.DataFrame of prices data
+    :param n: int number of periods to look back 
+    :return: pandas.Series of moving averages
+    """
+    catch_errors.check_for_period_error(data, n)
+    MA = pd.Series(data['Close'].rolling(n, min_periods=n).mean(), name='SMA_' + str(n))
+    return MA
+
+
+def exponential_moving_average(data, n):
+    """
+    Calculate the expenential  moving averge af the close price of a stock over n 
+    :param data: pandas.DataFrame
+    :param n: number of periods to look back
+    :return: pandas.Series of exponential moving average
+    """
+    catch_errors.check_for_period_error(data, n)
+    EMA = pd.Series(df['Adj Close'].ewm(span=n, min_periods=n).mean(), name='EMA_' + str(n))
+    return EMA
+
+
+def momentum(data, n):
+    """
+    Calculate the Momentum af the close price of a stock over n 
+    :param data: pandas.DataFrame 
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    catch_errors.check_for_period_error(data, n)
+    Momentum = pd.Series(data['Adj Close'].diff(n), name='Momentum_' + str(n))
+    return Momentum
+
+def price_SMA_ratio(data, n):
+	"""
+	Calculate the price over SMA ratio
+	:param data: pandas.DataFrame of prices data
+    :param n: int number of periods to look back 
+    :return: pandas.Series 
+
+	"""
+	catch_errors.check_for_period_error(data, n)
+	SMA = simple_moving_average(data, n)
+	ratio = data['Adj Close'] / SMA
+	return ratio
+
+
+def daily_returns(data):
+	"""
+	Calculate the daily returns
+	"""
+	daily_rets = pd.Series(data['Adj Close'].div(data['Adj Close'].shift(1))-1, name='daily_rets')
+	return daily_rets
+
+
+def relative_strength_index(data, lookback):
+	"""
+	Calculate the relative strength index 
+	:param data: pandas.DataFrame of prices data
+    :param lookback: int number of periods to look back 
+    :return: pandas.Series 
+
+	"""
+	catch_errors.check_for_period_error(data, lookback)
+	daily_rets = daily_returns(data)
+	rsi_list=[np.nan] * lookback
+	for day in range(lookback,data['Adj Close'].shape[0]):
+		up_gain = daily_rets[day - lookback+1:day+1].where(daily_rets >= 0).sum()
+		down_loss = -1 * daily_rets.iloc[day - lookback+1:day+1].where(daily_rets<0).sum()
+		if down_loss == 0:
+			rsi_list.append(100)
+		else:
+			rs = (up_gain / lookback) / (down_loss / lookback)
+			rsi_list.append(100 - (100/(1+rs)))
+	rsi = pd.Series(rsi_list, index = data.index)
+	return rsi
+
+
+
+def stochastic_oscillator(data, n):
+
+    catch_errors.check_for_period_error(data, n)
+
+    percent_k = [100 * (data['Adj Close'] - data['Adj Close'].rolling(n, min_periods=n).min()) / 
+    	(data['Adj Close'].rolling(n, min_periods=n).max() - data['Adj Close'].rolling(n, min_periods=n).min())]
+
+
+    return percent_k
+
+
+
+
